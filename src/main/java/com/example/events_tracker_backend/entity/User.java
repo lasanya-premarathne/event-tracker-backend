e241c83ac91fbaed09fd5dfbd16d79452d5ce1f0
lasanya-premarathne <kldpremarathne@gmail.com>
2025-07-30 02:05:37 +0530

    entity/user - created

---
+++ src/main/java/com/example/events_tracker_backend/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.events_tracker_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data //@Getter/@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+}
