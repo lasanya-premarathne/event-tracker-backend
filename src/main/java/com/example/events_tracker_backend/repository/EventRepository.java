6721b4233c5b54f054a38856dbe09c64467b878d
lasanya-premarathne <kldpremarathne@gmail.com>
2025-07-30 02:06:34 +0530

    repository/event - created

---
+++ src/main/java/com/example/events_tracker_backend/repository/EventRepository.java
@@ -0,0 +1,18 @@
+package com.example.events_tracker_backend.repository;
+
+import com.example.events_tracker_backend.entity.Event;
+import com.example.events_tracker_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+    /**
+     * Finds all the Events by the given username.
+     * @param user The userId to search for
+     * @return a list of Events if events exist for the given user, empty list otherwise
+     */
+    List<Event> findAllByCreatedByIs(User user);
+}
